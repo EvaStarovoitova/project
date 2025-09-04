7ac572ec6027e73bb21929e3d21dc43a5c0b0d35
Eva Starovoitova <ewa.starov@gmail.com>
2025-09-04 16:34:52 +0300

    add

--- app/courses/urls.py
+++ app/courses/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path 
-from .views import CoursesCreateView, CoursesListView
+from .views import CoursesCreateView, CoursesListView, CoursesDetailView, LanguageListView
 
 
 urlpatterns=[
   
-    path('', CoursesListView.as_view(), name='home'),  # корневой URL
+    path('', CoursesListView.as_view(), name='home'),
     path('create/', CoursesCreateView.as_view(), name="courses_create"),
     path('courses/', CoursesListView.as_view(), name="courses_list"),
+    path('courses/<int:pk>/', CoursesDetailView.as_view(), name="courses_detail"),
+    path('language/',  LanguageListView.as_view(), name="language"),
 
 ]
--- app/courses/views.py
+++ app/courses/views.py
@@ -1,23 +1,11 @@
 
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
-# Create your views here.
-from .models.courses import Course
+from django.views.generic import ListView, CreateView, DetailView
+
+from .models.courses import Course, Language
 from django.contrib import messages
 
-
-
-#def courses_list(request):
-#    courses=Course.objects.order_by("-created_at")
-#    form=CourseForm()
-#    return render(request, 'courses/courses_list.html', {"courses":courses, "form":form})
-   
-
-# class BookDetails(DetailView):
-#   model = Book
-    #template_name="books/book_details.html"
-    #context_object_name="book"
 
 
 
@@ -41,23 +29,19 @@ class CoursesListView(ListView):
     template_name = "courses/courses_list.html"
     context_object_name = "courses"
 
-#class CoursesCreateView(FormView):
-#    template_name = "courses/courses_create.html"
-#    form_class = CoursesForm  
-#    success_url = reverse_lazy("courses_list")
-#    
-#    def form_valid(self, form):
-#      
-#        form.save()
-#        return super().form_valid(form)
 
-# class BookList(ListView):
-# model = Book
- #template_name="books/book_list.html"
- #context_object_name="books"
+class CoursesDetailView(DetailView):
+    model = Course
+    template_name = "courses/courses_detail.html"
+    context_object_name = "course"
 
- #по умолчанию template_name="название приложения/название модели_название view.html "
- # поумолчанию context_object_name="object_название view (list)"
+class LanguageList(ListView):
+    model=Language
 
-
-
+class LanguageListView(ListView):
+    model = Language
+    template_name = 'courses/language.html'
+    context_object_name = 'languages'
+    
+    def get_queryset(self):
+        return Language.objects.filter(is_active=True).order_by('name')
